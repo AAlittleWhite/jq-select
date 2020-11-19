38ae6274755621dbe9a214e76d5e2730d6ec60ff
baitaiyan <1546744970@qq.com>
2020-11-19 18:46:31 +0800

    'e优化'

--- ySelect.js
+++ ySelect.js
@@ -29,8 +29,13 @@
                 this.reload(); 
             }, 
             reload: function () {
-                if (this.settings.showSearch) { 
-                    var search = '<div class="fs-search"><input type="search" placeholder="' + this.settings.searchText + '" /><span class="fs-selectAll"><i class="fa fa-check-square-o"></i></span></div>'; 
+                if (this.settings.showSearch) {
+                    var multiple = this.$select.is('[multiple]') ? ' multiple' : ''; 
+                    if(multiple !== ''){
+                        var search = '<div class="fs-search"><input type="search" placeholder="' + this.settings.searchText + '" /><span class="fs-selectAll"><i class="fa fa-check-square-o"></i></span></div>'; 
+                    }else{
+                        var search = '<div class="fs-search"><input type="search" placeholder="' + this.settings.searchText + '" /><span class="clear">清空</span></div>';
+                    }
                     this.$wrap.find('.fs-dropdown').prepend(search); 
                 }
                 var choices = this.buildOptions(this.$select); 
@@ -44,19 +49,24 @@
             }, 
             buildOptions: function ($element) {
                 var $this = this; 
-                var choices = ''; 
-                $element.children().each(function (i, el) {
-                    var $el = $(el); 
-                    if ('optgroup' == $el.prop('nodeName').toLowerCase()) { 
-                        choices += '<div class="fs-optgroup">'; 
-                        choices += '<div class="fs-optgroup-label">' + $el.prop('label') + '</div>'; 
-                        choices += $this.buildOptions($el); 
-                        choices += '</div>'; 
-                    } else { 
-                        var selected = $el.is('[selected]') ? ' selected' : ''; 
-                        choices += '<div class="fs-option' + selected + '" data-value="' + $el.prop('value') + '"><span class="fs-checkbox"><i></i></span><div class="fs-option-label">' + $el.html() + '</div></div>'; 
-                    }
-                }); 
+                var choices = '';
+                if($element.children().length > 0){
+                    $element.children().each(function (i, el) {
+                        var $el = $(el); 
+                        if ('optgroup' == $el.prop('nodeName').toLowerCase()) { 
+                            choices += '<div class="fs-optgroup">'; 
+                            choices += '<div class="fs-optgroup-label">' + $el.prop('label') + '</div>'; 
+                            choices += $this.buildOptions($el); 
+                            choices += '</div>'; 
+                        } else { 
+                            var selected = $el.is('[selected]') ? ' selected' : ''; 
+                            choices += '<div class="fs-option' + selected + '" data-value="' + $el.prop('value') + '"><span class="fs-checkbox"><i></i></span><div class="fs-option-label">' + $el.html() + '</div></div>'; 
+                        }
+                    }); 
+                }else{
+                    choices =  "<p class='empty'>暂无数据</p>"
+                } 
+                
                 return choices;
             }, 
             reloadDropdownLabel: function () {
@@ -91,6 +101,10 @@
             }
         });
     }
+    // 是否按住shift键位
+    var isShift = false;
+    // 默认标记第一个为默认标记项（按住shift键可选中区间范围） 
+    var flag = 0;
     window.ySelect = { 'active': null, 'idx': -1 }; 
     function setIndexes($wrap) { 
         $wrap.find('.fs-option:not(.hidden)').each(function (i, el) { $(el).attr('data-index', i); 
@@ -112,7 +126,7 @@
     }
     function isAllSelected() {
         let selected = [];
-        let noHidden = [] 
+        let noHidden = []; 
         let allOption = $('.fs-options').find(".fs-option")
         allOption.each(function(){
             if(!$(this).hasClass('hidden')){
@@ -129,26 +143,40 @@
         }
         $('.fs-wrap').find('select').ySelect('reloadDropdownLabel'); 
     }
-    $(document).on('click', '.fs-selectAll', function () { 
-        $(this).parent().next().find('.fs-option.selected').click(); 
+    $(document).on('click', '.fs-selectAll', function () {
         var curOption = $(this).parent().next().find('.fs-option')
         for (let index = 0; index < curOption.length; index++) {
             let element = curOption[index];
             if(!$(element).hasClass('hidden')){
-                $(element).click();
+                $(element).addClass('selected')
             } 
         }
         $(this).addClass('selected');
+        $(this).closest('.fs-wrap').find('select').ySelect('reloadDropdownLabel');
     }); 
     $(document).on('click', '.fs-selectAll.selected', function () { 
-        $(this).parent().next().find('.fs-option.selected').click(); 
+        var curOption = $(this).parent().next().find('.fs-option')
+        for (let index = 0; index < curOption.length; index++) {
+            let element = curOption[index];
+            if(!$(element).hasClass('hidden')){
+                $(element).removeClass('selected'); 
+            } 
+        } 
         $(this).removeClass('selected'); 
+        $(this).closest('.fs-wrap').find('select').ySelect('reloadDropdownLabel');
     }); 
     $(document).on('click', '.fs-option', function () {
-        var $wrap = $(this).closest('.fs-wrap'); 
-        if ($wrap.hasClass('multiple')) { 
+        var $wrap = $(this).closest('.fs-wrap');
+        if ($wrap.hasClass('multiple')) {
+            if(isShift){
+                let min = flag <= $(this).index() ? flag : $(this).index()
+                let max = flag <= $(this).index() ? $(this).index() : flag
+                for(let i = min + 1; i < max; i++){
+                    $wrap.find('.fs-option').eq(i).toggleClass('selected');
+                }
+            }
+            flag = $(this).index() 
             var selected = [];
-            let noHidden = [] 
             $(this).toggleClass('selected'); 
             $wrap.find('.fs-option.selected').each(function (i, el) { 
                 selected.push($(el).attr('data-value')); 
@@ -159,7 +187,7 @@
             var selected = $(this).attr('data-value'); 
             $wrap.find('.fs-option').removeClass('selected'); 
             $(this).addClass('selected'); 
-            $wrap.find('.fs-dropdown').hide(); 
+            $wrap.find('.fs-dropdown').addClass('hidden');  
         }
         $wrap.find('select').val(selected); 
         $wrap.find('select').ySelect('reloadDropdownLabel'); 
@@ -188,12 +216,22 @@
         // 判断是否是全部选中
         isAllSelected()
     }); 
+    $(document).on('click', '.clear', function(){
+        var $wrap = $(this).closest('.fs-wrap');
+        var element = $wrap.find('.fs-options').find('.fs-option.selected')
+        if(element){
+            $(element).removeClass('selected'); 
+        }
+        $('.fs-wrap').find('.fs-label').text('请选择')
+        $wrap.find('.fs-search').find("input[type='search']").val('')
+    })
     $(document).on('click', function (e) {
         var $el = $(e.target); 
         var $wrap = $el.closest('.fs-wrap'); if (0 < $wrap.length) {
             if ($el.hasClass('fs-label') || $el.hasClass('fs-arrow')) {
                 window.ySelect.active = $wrap; 
-                var is_hidden = $wrap.find('.fs-dropdown').hasClass('hidden'); $('.fs-dropdown').addClass('hidden'); 
+                var is_hidden = $wrap.find('.fs-dropdown').hasClass('hidden'); 
+                $('.fs-dropdown').addClass('hidden'); 
                 if (is_hidden) { 
                     $wrap.find('.fs-dropdown').removeClass('hidden'); 
                 } else { 
@@ -207,6 +245,10 @@
         }
     }); 
     $(document).on('keydown', function (e) {
+        var e = window.event || e;
+		if( e.keyCode == 16 ){
+			isShift = true;
+		}
         var $wrap = window.ySelect.active; 
         if (null === $wrap) { 
             return; 
@@ -233,7 +275,39 @@
             $('.fs-dropdown').addClass('hidden'); 
             window.ySelect.active = null; 
         }
-    }); 
+    });
+    $(document).on('keyup', function(e){
+        var e = window.event || e
+        if( e.keyCode == 16 ){
+			isShift = false;
+		}
+    })
+    $.fn.reload = function(){
+        let $element = $('.fs-wrap').find('select')
+        $('.fs-wrap').find('.fs-label').text('请选择')
+        var $this = this; 
+        var choices = '';
+        if($element.children().length > 0){
+            $element.children().each(function (i, el) {
+                var $el = $(el); 
+                if ('optgroup' == $el.prop('nodeName').toLowerCase()) { 
+                    choices += '<div class="fs-optgroup">'; 
+                    choices += '<div class="fs-optgroup-label">' + $el.prop('label') + '</div>'; 
+                    choices += $this.buildOptions($el); 
+                    choices += '</div>'; 
+                } else { 
+                    var selected = $el.is('[selected]') ? ' selected' : ''; 
+                    choices += '<div class="fs-option' + selected + '" data-value="' + $el.prop('value') + '"><span class="fs-checkbox"><i></i></span><div class="fs-option-label">' + $el.html() + '</div></div>'; 
+                }
+            }); 
+        }else{
+            choices =  "<p class='empty'>暂无数据</p>"
+        } 
+        $element.empty()
+        $('.fs-wrap').find('.fs-options').html(choices);
+        $('.fs-wrap').find('.fs-selectAll').removeClass('selected') 
+        $('.fs-wrap').find('.fs-search').find("input[type='search']").val('')
+    }
     $.fn.ySelectedValues = function () {
         var result = [];
         var $selects = this.parent().find('.fs-dropdown').find('.fs-options').find(".fs-option");
@@ -241,9 +315,6 @@
             if($($selects[i]).hasClass('selected')){
                 result.push($($selects[i]).attr('data-value')); 
             }
-            // if(!$($selects[i]).hasClass('hidden') && $($selects[i]).hasClass('selected')){
-            //     result.push($($selects[i]).attr('data-value')); 
-            // }
         }
         return result;
     }
